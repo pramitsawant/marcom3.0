fd2eecd9da5731a1e5a5c335aecdd9a203f01379
Pramit Sawant <pramitsawant11@gmail.com>
2021-04-03 15:46:30 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="min-h-screen flex flex-col font-sans">
+      <section class="flex-grow p-8 text-2xl">
+        <Nuxt />
+      </section>
+    <Footer/>
+    </div>
+
+    
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from "~/components/layout-v2/navbar.vue";
+import Footer from "~/components/layout-v2/footer.vue";
+
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+<style>
+
+</style>
